079fbae2f14c20e349e20ef6516c0f667561f2a9
TKMhub <111867865+TKMhub@users.noreply.github.com>
2024-12-16 21:20:44 +0900

    init

--- app/components/Feed.tsx
+++ app/components/Feed.tsx
@@ -28,7 +28,6 @@ export default function Feed() {
         </CardContent>
       </Card>
 
-      {/* Posts */}
       {posts.map((post) => (
         <Card key={post} className="mb-6">
           <CardHeader>
